ba83be69d0d7669da3addc85d60df8376fee4a9d
ramanidharan143 <kramanidharan7@gmail.com>
2025-09-21 22:51:21 +0530

    Create EvenInRange.java

---
+++ EvenInRange.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class EvenInRange {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter start of range: ");
+        int start = sc.nextInt();
+        System.out.print("Enter end of range: ");
+        int end = sc.nextInt();
+
+        System.out.println("Even numbers between " + start + " and " + end + ":");
+        for (int i = start; i <= end; i++) {
+            if (i % 2 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+        sc.close();
